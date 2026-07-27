df8bd766e834afcaf0e23c36a862d6e6bd7032ce
ferdotexe <fernandosilva348@gmail.com>
2026-07-26 23:24:08 -0300

    initial commit

---
+++ src/main/java/com/helpdeskall/HelpDeskAll/HelpDeskAllApplication.java
@@ -0,0 +1,13 @@
+package com.helpdeskall.HelpDeskAll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpDeskAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpDeskAllApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helpdeskall/HelpDeskAll/HelpDeskAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helpdeskall.HelpDeskAll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpDeskAllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
